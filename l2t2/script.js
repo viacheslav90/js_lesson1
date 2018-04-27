dc425df319431ae34d4ac1032fa93313185459f6
Slavik <v.dontsov@gmail.com>
2018-04-27 09:09:40 +0300

    homework 2

--- l2t2/script.js
+++ l2t2/script.js
@@ -2,30 +2,20 @@ var arr = [ 1, 2, 1, 2, 3.14, 4, 2, 1];
 
 function calculateRepsQuantity(sourceArray) {
 
-    var resultArray = [];
-
-    var countNumbers = function (arr, number) {
-        var counter = 0;
+    resultArray = sourceArray.map(function(element, index, array) {
+        var quantity = 0;
         arr.forEach(function (element) {
-            if (element === number)
-                counter++;
+            if (element === array[index])
+                quantity++;
         });
-        return counter;
-    };
-
-    for (var i = 0; i < sourceArray.length; i++) {
-        var quantity = countNumbers(sourceArray, sourceArray[i]);
-        var result = sourceArray[i] + " - " + quantity;
-        resultArray.push(result);
-    }
-
-    resultArray = resultArray.filter(function (element, index) {
-        return resultArray.indexOf(element) === index;
+        return element + " - " + quantity;
+    }).filter(function (element, index, array) {
+        return array.indexOf(element) === index;
     });
 
     return resultArray;
 }
 
-console.log(calculateRepsQuantity(arr));
+console.log("Calculating numbers quantity: " + calculateRepsQuantity(arr));
 
 
--- l2t3/script.js
+++ l2t3/script.js
@@ -8,32 +8,24 @@ function calculateRepsQuantity(sourceArray) {
         return number < 0;
     });
 
-    var resultArray = [];
-
-    var countNumbers = function (arr, number) {
-        var counter = 0;
+    var calculateRepsQuantity = function(sourceArray) {
+    resultArray = sourceArray.map(function(element, index, array) {
+        var quantity = 0;
         arr.forEach(function (element) {
-            if (element === number)
-                counter++;
+            if (element === array[index])
+                quantity++;
         });
-        return counter;
-    };
-
-    for (var i = 0; i < sourceArray.length; i++) {
-        var quantity = countNumbers(sourceArray, sourceArray[i]);
-        var result = sourceArray[i] + " - " + quantity;
-        resultArray.push(result);
-    }
-
-    resultArray = resultArray.filter(function (element, index) {
-        return resultArray.indexOf(element) === index;
+        return element + " - " + quantity;
+    }).filter(function (element, index, array) {
+        return array.indexOf(element) === index;
     });
 
+    return resultArray;
+    }
 
     console.log("Positive array: " + positiveArray);
     console.log("Negative array: " + negativeArray);
-    console.log("Calculating numbers quantity: " + resultArray);
-
+    console.log("Calculating numbers quantity: " + calculateRepsQuantity(sourceArray));
 }
 
 var arr = [ 1, 2, 1, 2, 3.14, 4, 2, 1];
